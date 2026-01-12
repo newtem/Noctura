9c39ed8c79b092851e46faa3721c6d9e429aae89
newtem <72638890+newtem@users.noreply.github.com>
2026-01-12 19:39:28 +0900

    Added Noctura updater
    main noctura has not been uploaded yet

---
+++ Noctura_Updater/Updater.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Noctura_Updater
+{
+    partial class Updater
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updater));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(150, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Noctura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(150)))), ((int)(((byte)(200)))));
+            this.label2.Location = new System.Drawing.Point(153, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Updater 1.0";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.ForeColor = System.Drawing.Color.White;
+            this.StatusLabel.Location = new System.Drawing.Point(143, 320);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(165, 24);
+            this.StatusLabel.TabIndex = 2;
+            this.StatusLabel.Text = "Updater started";
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.AutoSize = true;
+            this.ProgressLabel.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProgressLabel.ForeColor = System.Drawing.Color.White;
+            this.ProgressLabel.Location = new System.Drawing.Point(77, 288);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(297, 24);
+            this.ProgressLabel.TabIndex = 3;
+            this.ProgressLabel.Text = "[--------------------] 0%";
+            // 
+            // Updater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(450, 582);
+            this.ControlBox = false;
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Updater";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Updater";
+            this.Load += new System.EventHandler(this.Updater_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label ProgressLabel;
+    }
+}
+
---
+++ Noctura_Updater/Updater.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+//using System.Drawing;
+
+namespace Noctura_Updater
+{
+    public partial class Updater : Form
+    {
+        private const string downloadUrl = "https://github.com/newtem/Noctura/releases/latest/download/Noctura.exe";
+        private const string tempExe = "NocturaNew.exe";
+        private const string targetExe = "Noctura.exe";
+
+        private const int progressBar = 20;
+        private WebClient webClient;
+
+        public Updater()
+        {
+            InitializeComponent();
+        }
+
+        private void StartDownload()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            webClient = new WebClient();
+            webClient.DownloadProgressChanged += DownloadProgress;
+            webClient.DownloadFileCompleted += DownloadNocturaCompleted;
+
+            StatusLabel.Text = "Downloading Noctura...";
+            LabelsSetCenter();
+            webClient.DownloadFileAsync(new Uri(downloadUrl), tempExe);
+        }
+
+        private void DownloadProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
+            int percent = e.ProgressPercentage;
+            int filled = (percent * progressBar) / 100;
+            int empty = progressBar - filled;
+
+            var bar = new StringBuilder();
+            bar.Append('[');
+            bar.Append(new string('#', filled));
+            bar.Append(new string('-', empty));
+            bar.Append($"] {percent}%");
+
+            ProgressLabel.Text = bar.ToString();
+        }
+
+        private void DownloadNocturaCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("Download failed:\n" + e.Error.Message);
+                return;
+            }
+
+            StatusLabel.Text = "Applying updates...";
+            LabelsSetCenter();
+            ApplyUpdate();
+        }
+
+        private void ApplyUpdate()
+        {
+            try
+            {
+                foreach (var proc in Process.GetProcessesByName(
+                    Path.GetFileNameWithoutExtension(targetExe)))
+                {
+                    proc.WaitForExit(); // wait for exit Noctura.exe
+                }
+
+                if (File.Exists(targetExe))
+                    File.Delete(targetExe);
+
+                File.Move(tempExe, targetExe);
+
+                StatusLabel.Text = "Update completed.";
+                LabelsSetCenter();
+                Process.Start(targetExe);
+
+                // End of update. updater go to bed
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update failed:\n" + ex.Message);
+            }
+        }
+
+        private void Updater_Load_1(object sender, EventArgs e)
+        {
+            StatusLabel.Text = "Preparing to download...";
+            ProgressLabel.Text = "[--------------------] 0%";
+            StartDownload();
+            LabelsSetCenter();
+        }
+
+        private void LabelsSetCenter()
+        {
+            StatusLabel.Left = (this.ClientSize.Width - StatusLabel.Width) / 2;
+            ProgressLabel.Left = (this.ClientSize.Width - ProgressLabel.Width) / 2;
+        }
+    }
+}
